d0fd4f9089070b07ddd284700dfdb0b6a8e054d7
Nancy <nancyrs22@hotmail.com>
2018-01-25 13:42:00 -0500

    Clase 2

---
+++ p1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n1, n2;
+	string s;
+	
+	cin>>n1>>n2;
+	cin>>s;
+
+	cout<< s <<" : "<< n1 + n2;
+	return 0;
+}
+
---
+++ p2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n1, n2;
+	string s;
+	
+	cin>>n1>>n2;
+	cin>>s;
+
+	cout<< n1 * n2 <<" = "<< s;
+	return 0;
+}
